722001475e5a0f9baa27e44d0e8df49d4793f6d5
陈依苓 <844621236@qq.com>
2024-08-31 18:54:33 +0800

    feat: 外网link配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   projectName: "gugujiang-t.github.io", // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
